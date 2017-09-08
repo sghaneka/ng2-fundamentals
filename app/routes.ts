4d19b82f4ef4cc0d5abce0c45d0b7845c18f2460
samgh <samgh@DESKTOP-V2LTOKR>
2017-09-08 07:55:41 -0400

    small change

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router'
 import { EventsListComponent } from './events/events-list.component'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 
-export const appRoutes = [
+export const appRoutes:Routes = [
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent },
     { path: '', redirectTo: '/events', pathMatch: 'full'}
